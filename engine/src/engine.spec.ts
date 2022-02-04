010444add9efe7611fdaa3a96aaf27b2274c08ea
Lucas Mello <lucas@bitka.com.br>
2022-02-04 10:48:06 -0300

    ✅ (engine) test for isUndo

--- engine/src/engine.spec.ts
+++ engine/src/engine.spec.ts
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import { ended, move, reconstructState, setup } from './engine';
 import GermanyRecharged from './fixtures/GermanyRecharged.json';
 import supply from './fixtures/supply.json';
+import undo from './fixtures/undo.json';
 import USAOriginal from './fixtures/USAOriginal.json';
 import { GameOptions, MapName, Variant } from './gamestate';
 import { Move } from './move';
@@ -127,6 +128,26 @@ describe('Engine', () => {
         const game = supply;
 
         const G = reconstructState(game as any, game.log.length - 1);
+
+        expect(ended(G)).to.be.false;
+    });
+
+    it('should allow invalid move when isUndo is true', () => {
+        const game = undo;
+        const options: GameOptions = {
+            fastBid: game.options.fastBid,
+            map: game.options.map as MapName,
+            showMoney: game.options.showMoney,
+            variant: game.options.variant as Variant,
+        };
+
+        let G = setup(game.players.length, options, game.seed, game.knownPowerPlantDeck, game.map);
+
+        for (const item of game.log) {
+            if (item.type === 'move') {
+                G = move(G, item.move! as Move, item.player!, true);
+            }
+        }
 
         expect(ended(G)).to.be.false;
     });
